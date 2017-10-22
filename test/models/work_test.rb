6c5d5f15cfd4fa649dcc61ab8896babcac7af061
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-22 14:17:32 -0700

    Passing all work model tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -20,10 +20,12 @@ describe Work do
   end
 
   describe "validations" do
+    let(:user) {users(:dan)}
+
     it "allows the three valid categories" do
       valid_categories = ['album', 'book', 'movie']
       valid_categories.each do |category|
-        work = Work.new(title: "test", category: category)
+        work = Work.new(title: "test", category: category, user_id: user.id)
         work.valid?.must_equal true
       end
     end
@@ -31,7 +33,7 @@ describe Work do
     it "fixes almost-valid categories" do
       categories = ['Album', 'albums', 'ALBUMS', 'books', 'mOvIeS']
       categories.each do |category|
-        work = Work.new(title: "test", category: category)
+        work = Work.new(title: "test", category: category, user_id: user.id)
         work.valid?.must_equal true
         work.category.must_equal category.singularize.downcase
       end
@@ -40,14 +42,14 @@ describe Work do
     it "rejects invalid categories" do
       invalid_categories = ['cat', 'dog', 'phd thesis', 1337, nil]
       invalid_categories.each do |category|
-        work = Work.new(title: "test", category: category)
+        work = Work.new(title: "test", category: category, user_id: user.id)
         work.valid?.must_equal false
         work.errors.messages.must_include :category
       end
     end
 
     it "requires a title" do
-      work = Work.new(category: 'ablum')
+      work = Work.new(category: 'ablum', user_id: user.id)
       work.valid?.must_equal false
       work.errors.messages.must_include :title
     end
@@ -55,35 +57,41 @@ describe Work do
     it "requires unique names w/in categories" do
       category = 'album'
       title = 'test title'
-      work1 = Work.new(title: title, category: category)
+      work1 = Work.new(title: title, category: category, user_id: user.id)
       work1.save!
 
-      work2 = Work.new(title: title, category: category)
+      work2 = Work.new(title: title, category: category, user_id: user.id)
       work2.valid?.must_equal false
       work2.errors.messages.must_include :title
     end
 
     it "does not require a unique name if the category is different" do
       title = 'test title'
-      work1 = Work.new(title: title, category: 'album')
+      work1 = Work.new(title: title, category: 'album', user_id: user.id)
       work1.save!
 
-      work2 = Work.new(title: title, category: 'book')
+      work2 = Work.new(title: title, category: 'book', user_id: user.id)
       work2.valid?.must_equal true
+    end
+
+    it "requires a valid user id" do
+      work1 = Work.new(title: "title", category: 'album')
+      work1.save.must_equal false
     end
   end
 
   describe "vote_count" do
+    let(:user) {users(:dan)}
     it "defaults to 0" do
-      work = Work.create!(title: "test title", category: "movie")
+      work = Work.create!(title: "test title", category: "movie", user_id: user.id)
       work.must_respond_to :vote_count
       work.vote_count.must_equal 0
     end
 
     it "tracks the number of votes" do
-      work = Work.create!(title: "test title", category: "movie")
+      work = Work.create!(title: "test title", category: "movie", user_id: user.id)
       4.times do |i|
-        user = User.create!(username: "user#{i}")
+        user = User.create!(username: "user#{i}", uid: (100 + i), provider: "github", email: "#{i}@test.com")
         Vote.create!(user: user, work: work)
       end
       work.vote_count.must_equal 4
@@ -97,13 +105,13 @@ describe Work do
       # Create users to do the voting
       test_users = []
       20.times do |i|
-        test_users << User.create!(username: "user#{i}")
+        test_users << User.create!(username: "user#{i}", uid: (100 + i), email: "#{i}@test.com", provider: "github")
       end
 
       # Create media to vote upon
       Work.where(category: "movie").destroy_all
       8.times do |i|
-        work = Work.create!(category: "movie", title: "test movie #{i}")
+        work = Work.create!(category: "movie", title: "test movie #{i}", user_id: test_users[i].id)
         vote_count = rand(test_users.length)
         test_users.first(vote_count).each do |user|
           Vote.create!(work: work, user: user)
@@ -132,14 +140,15 @@ describe Work do
     it "returns at most 10 items" do
       movies = Work.top_ten("movie")
       movies.length.must_equal 8
+      user = users(:dan)
 
-      Work.create(title: "phase 2 test movie 1", category: "movie")
+      Work.create(title: "phase 2 test movie 1", category: "movie", user_id: user.id)
       Work.top_ten("movie").length.must_equal 9
 
-      Work.create(title: "phase 2 test movie 2", category: "movie")
+      Work.create(title: "phase 2 test movie 2", category: "movie", user_id: user.id)
       Work.top_ten("movie").length.must_equal 10
 
-      Work.create(title: "phase 2 test movie 3", category: "movie")
+      Work.create(title: "phase 2 test movie 3", category: "movie", user_id: user.id)
       Work.top_ten("movie").length.must_equal 10
     end
   end
